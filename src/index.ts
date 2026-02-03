2a518bc2dd9f4bcf7a9e6859b998af4efcb2fbcc
Spatulox <M78stormtrooper@laposte.net>
2026-02-03 14:53:20 +0100

    Exports core classes and types
    Exposes necessary classes and types for external use.
    
    This allows consumers to readily access and utilize key
    functionalities related to bot management, Discord utilities,
    message handling, and time calculations.

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+export { Bot } from "./bot/Bot.js"
+
+export { ChannelManager } from './utils/discord/ChannelManager.js';
+export { UserManager } from './utils/discord/UserManager.js';
+export { RoleManager } from './utils/discord/RoleManager.js';
+export { InviteManager } from './utils/discord/InviteManager.js';
+
+export { EmbedManager, EmbedColor } from './utils/messages/EmbedManager.js';
+export { WebhookManager } from './utils/messages/WebhookManager.js';
+
+export { Time } from "./utils/times/UnitTime.js"
+
+export type { BotConfig, RandomBotActivity } from './bot/Bot.js';
