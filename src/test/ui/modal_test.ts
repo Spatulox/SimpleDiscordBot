c1af80119f2511a1d095177af71226a62165c522
Spatulox <M78stormtrooper@laposte.net>
2026-02-19 18:06:48 +0100

    Enables modal tests and removes unused function
    Uncomments modal tests to enable functionality.
    
    Removes the `toInteraction` function as it is no longer needed.

--- src/test/ui/modal_test.ts
+++ src/test/ui/modal_test.ts
@@ -19,24 +19,24 @@ export async function modal_test(interaction: ChatInputCommandInteraction) {
     ])
     interaction.showModal(modal)
 
-    /*const modal2 = ModalManager.simple("mod2", "Simple Title", {type: ModalFieldType.SHORT, label: "Simple"})
-    interaction.showModal(modal2)*/
+    const modal2 = ModalManager.simple("mod2", "Simple Title", {type: ModalFieldType.SHORT, label: "Simple"})
+    interaction.showModal(modal2)
 
-    /*const modal3 = ModalManager.titleDescription("mod3", "TitreDesc Title", {label: "Title Desc"}, {label: "Desc desc"})
-    interaction.showModal(modal3)*/
+    const modal3 = ModalManager.titleDescription("mod3", "TitreDesc Title", {label: "Title Desc"}, {label: "Desc desc"})
+    interaction.showModal(modal3)
 
-    /*const modal4 = ModalManager.date("mod4", "Date Title")
-    interaction.showModal(modal4)*/
+    const modal4 = ModalManager.date("mod4", "Date Title")
+    interaction.showModal(modal4)
 
-    /*const modal5 = ModalManager.number("mod5", "Number Title")
-    interaction.showModal(modal5)*/
+    const modal5 = ModalManager.number("mod5", "Number Title")
+    interaction.showModal(modal5)
 
-    /*const modal6 = ModalManager.phone("mod6", "Phone Title")
-    interaction.showModal(modal6)*/
+    const modal6 = ModalManager.phone("mod6", "Phone Title")
+    interaction.showModal(modal6)
 
-    /*interaction.reply({
+    interaction.reply({
         content: "Hello, world!",
         embeds: [modal, modal2, modal3, modal4, modal5, modal6],
         components: []
-    })*/
+    })
 }
