b459aa5541d041bcb7206d1acab45fb4ba446d3c
Spatulox <M78stormtrooper@laposte.net>
2026-02-19 18:06:27 +0100

    Adds button message conversion methods
    Adds methods to convert buttons and action rows into message and interaction options, including support for ephemeral messages.
    
    This change provides a cleaner and more consistent way to send buttons in both regular messages and interaction responses.

--- src/test/ui/button_test.ts
+++ src/test/ui/button_test.ts
@@ -1,7 +1,8 @@
-import {ChatInputCommandInteraction} from "discord.js";
+import {ChatInputCommandInteraction, InteractionReplyOptions} from "discord.js";
 import {ButtonManager} from "../../manager/interactions/ButtonManager";
+import {GuildManager} from "../../manager/guild/GuildManager";
 
-export function button_test(interaction: ChatInputCommandInteraction) {
+export async function button_test(interaction: ChatInputCommandInteraction) {
 
     const row = ButtonManager.row([
         ButtonManager.primary({ customId: 'confirm', label: 'Confirm', emoji: '✅' }),
@@ -11,8 +12,13 @@ export function button_test(interaction: ChatInputCommandInteraction) {
         ButtonManager.link( {label: 'Link', url:"https://google.com", emoji:'✅'}),
     ])
 
-    interaction.reply({
-        content: `test`,
-        components: [row]
-    })
+    const channel = await GuildManager.channel.text.find("1162047096220827831")
+    if (channel) {
+        await channel.send("--BASIC--")
+        await channel.send(ButtonManager.toMessage(ButtonManager.primary({ customId: 'confirm', label: 'Confirm', emoji: '✅' })))
+        await channel.send("--COMPLEX--")
+        await channel.send(ButtonManager.toMessage(row))
+    }
+
+    interaction.reply(ButtonManager.toInteraction(row) as InteractionReplyOptions)
 }
